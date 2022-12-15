3faa5905e1896730b27165d181131183f13c617d
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-12-15 16:01:16 +0200

    Day 15: memory-intensive but working part 2

--- src/main/java/juuxel/advent2022/Day15.java
+++ src/main/java/juuxel/advent2022/Day15.java
@@ -1,18 +1,13 @@
 package juuxel.advent2022;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.*;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.regex.Pattern;
 
 public final class Day15 {
     private static final Pattern PATTERN = Pattern.compile("^Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)$");
-    private static final ConcurrentLinkedQueue<Progress> progressQueue = new ConcurrentLinkedQueue<>();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         part1(args);
         part2(args);
     }
@@ -41,64 +36,63 @@ public final class Day15 {
         System.out.println(score);
     }
 
-    private static void part2(String[] args) throws Exception {
-        record SensorData(Point sensor, int closestDistance) {}
+    // Note: very memory-intensive! Requires at least 2G, probably 3G.
+    private static void part2(String[] args) {
         List<SensorData> sensors = new ArrayList<>();
+        Set<Point> perimeter = new HashSet<>();
         for (String line : args) {
-            readLine(line, (sensor, beacon) -> {
-                sensors.add(new SensorData(sensor, manhattan(sensor.x, sensor.y, beacon.x, beacon.y)));
-            });
+            readLine(line, sensors, perimeter);
         }
-        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 3);
-        Thread progressThread = new Thread(() -> {
-            float[] allProgress = new float[16];
+        Point result = findPart2Point(sensors, perimeter);
+        long score = 4_000_000L * (long) result.x + (long) result.y;
+        System.out.println(score);
+    }
 
-            while (true) {
-                Progress progress = progressQueue.poll();
-                if (progress != null) {
-                    allProgress[progress.index] = progress.x / 1_000_000f;
-
-                    float progressSum = 0;
-                    for (float v : allProgress) {
-                        progressSum += v;
-                    }
-                    progressSum *= 100f / 16f;
-                    System.out.printf("%.2f%%%n", progressSum);
+    private static Point findPart2Point(List<SensorData> sensors, Set<Point> perimeter) {
+        pointLoop:
+        for (Point point : perimeter) {
+            for (SensorData data : sensors) {
+                if (manhattan(data.sensor.x, data.sensor.y, point.x, point.y) <= data.beaconDist) {
+                    continue pointLoop;
                 }
             }
-        });
-        progressThread.setName("Progress Reporter");
-        progressThread.setDaemon(true);
-        progressThread.start();
-        int[] endpoints = new int[] { 0, 1_000_000, 2_000_000, 3_000_000, 4_000_001 };
-        int threadIndex = 0;
-        for (int xIndex = 0; xIndex < endpoints.length - 1; xIndex++) {
-            for (int yIndex = 0; yIndex < endpoints.length - 1; yIndex++) {
-                final int myIndex = threadIndex++;
-                int startX = endpoints[xIndex];
-                int endX = endpoints[xIndex + 1];
-                int startY = endpoints[yIndex];
-                int endY = endpoints[yIndex + 1];
-                CompletableFuture.runAsync(() -> {
-                    System.out.printf("Thread %s checking (%d, %d) -> (%d, %d)%n", Thread.currentThread().getName(), startX, startY, endX, endY);
-                    for (int x = startX; x < endX; x++) {
-                        progressQueue.add(new Progress(myIndex, x));
-                        point: for (int y = startY; y < endY; y++) {
-                            for (SensorData sensorData : sensors) {
-                                if (manhattan(sensorData.sensor.x, sensorData.sensor.y, x, y) <= sensorData.closestDistance) {
-                                    continue point;
-                                }
-                            }
-
-                            long score = (long) x * 4_000_000 + (long) y;
-                            System.out.println(score);
-                        }
-                    }
-                }, executor);
-            }
+            return point;
         }
-        if (!executor.awaitTermination(10L, TimeUnit.HOURS)) {
-            throw new IllegalStateException("Could not find answer in 10 hours");
+        throw new RuntimeException("Could not find answer!");
+    }
+
+    private static void readLine(String line, Collection<SensorData> sensors, Collection<Point> perimeter) {
+        readLine(line, (sensor, beacon) -> {
+            int manhattan = manhattan(sensor.x, sensor.y, beacon.x, beacon.y);
+            sensors.add(new SensorData(sensor, manhattan));
+            int x = sensor.x, y = sensor.y - (manhattan + 1);
+            putPoint(perimeter, x, y);
+            for (int i = 0; i < manhattan + 1; i++) {
+                x++;
+                y++;
+                putPoint(perimeter, x, y);
+            }
+            for (int i = 0; i < manhattan + 1; i++) {
+                x--;
+                y++;
+                putPoint(perimeter, x, y);
+            }
+            for (int i = 0; i < manhattan + 1; i++) {
+                x--;
+                y--;
+                putPoint(perimeter, x, y);
+            }
+            for (int i = 0; i < manhattan; i++) {
+                x++;
+                y--;
+                putPoint(perimeter, x, y);
+            }
+        });
+    }
+
+    private static void putPoint(Collection<Point> points, int x, int y) {
+        if (0 <= x && x <= 4_000_000 && 0 <= y && y <= 4_000_000) {
+            points.add(new Point(x, y));
         }
     }
 
@@ -123,10 +117,10 @@ public final class Day15 {
         }
     }
 
-    private record Progress(int index, int x) {
+    private record Point(int x, int y) {
     }
 
-    private record Point(int x, int y) {
+    private record SensorData(Point sensor, int beaconDist) {
     }
 
     private enum Tile {
